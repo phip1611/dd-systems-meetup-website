033b2a27b4ce9fe395dfd93cf530d9394a02fece
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-06-10 08:01:38 +0200

    nix: fix formatting

--- flake.nix
+++ flake.nix
@@ -7,45 +7,52 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, flake-parts, ... }@inputs:
+  outputs =
+    { self, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem = { config, pkgs, ... }: {
-        checks = {
-          fmt = pkgs.runCommandLocal "check-html-fmt"
-            {
-              src = self;
-              nativeBuildInputs = [ pkgs.nodePackages.prettier ];
-            } ''
-            prettier --check ${self}/public/index.html
-            touch $out
-          '';
-          typos = pkgs.runCommandLocal "run-typos"
-            {
-              src = self;
-              nativeBuildInputs = [ pkgs.typos ];
-            } ''
-            typos ${self}
-            touch $out
-          '';
-        };
-        devShells.default =
-          let
-            serve = pkgs.callPackage ./nix/serve.nix { };
-          in
-          pkgs.mkShell {
-            packages = with pkgs; [
-              fd
-              libwebp
-              nixfmt-rfc-style
-              # format: `$ prettier -w index.html`
-              nodePackages.prettier
-              serve
-              typos
-            ];
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          checks = {
+            fmt =
+              pkgs.runCommandLocal "check-html-fmt"
+                {
+                  src = self;
+                  nativeBuildInputs = [ pkgs.nodePackages.prettier ];
+                }
+                ''
+                  prettier --check ${self}/public/index.html
+                  touch $out
+                '';
+            typos =
+              pkgs.runCommandLocal "run-typos"
+                {
+                  src = self;
+                  nativeBuildInputs = [ pkgs.typos ];
+                }
+                ''
+                  typos ${self}
+                  touch $out
+                '';
           };
-        formatter = pkgs.nixfmt-rfc-style;
-      };
+          devShells.default =
+            let
+              serve = pkgs.callPackage ./nix/serve.nix { };
+            in
+            pkgs.mkShell {
+              packages = with pkgs; [
+                fd
+                libwebp
+                nixfmt-rfc-style
+                # format: `$ prettier -w index.html`
+                nodePackages.prettier
+                serve
+                typos
+              ];
+            };
+          formatter = pkgs.nixfmt-rfc-style;
+        };
       flake = {
         # Put your original flake attributes here.
       };
--- nix/serve.nix
+++ nix/serve.nix
@@ -1,4 +1,8 @@
-{ lib, live-server, writeShellScriptBin }:
+{
+  lib,
+  live-server,
+  writeShellScriptBin,
+}:
 
 writeShellScriptBin "serve" ''
   ${lib.getExe live-server} ./public --open --index
