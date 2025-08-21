b94b50b47cca5c246cc6a227c8e225ef0399305c
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-08-21 11:45:14 +0200

    flake: remove flake-parts dependency

--- flake.nix
+++ flake.nix
@@ -2,59 +2,66 @@
   description = "DD Systems Meetup Website";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs =
-    { self, flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    { self, ... }@inputs:
+    let
+      # Systems definition for dev shells and exported packages,
+      # independent of the NixOS configurations and modules defined here. I
+      # just use "every system" here to not restrict any user. However, it
+      # likely happens that certain packages don't build for/under certain
+      # systems.
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { config, pkgs, ... }:
-        {
-          checks = {
-            fmt =
-              pkgs.runCommandLocal "check-html-fmt"
-                {
-                  src = self;
-                  nativeBuildInputs = [ pkgs.nodePackages.prettier ];
-                }
-                ''
-                  prettier --check ${self}/public/index.html
-                  touch $out
-                '';
-            typos =
-              pkgs.runCommandLocal "run-typos"
-                {
-                  src = self;
-                  nativeBuildInputs = [ pkgs.typos ];
-                }
-                ''
-                  typos ${self}
-                  touch $out
-                '';
+
+      # Generates the typical per-system flake attributes.
+      forAllSystems =
+        function:
+        inputs.nixpkgs.lib.genAttrs systems (system: function inputs.nixpkgs.legacyPackages.${system});
+    in
+    {
+      checks = forAllSystems (pkgs: {
+        fmt =
+          pkgs.runCommandLocal "check-html-fmt"
+            {
+              src = self;
+              nativeBuildInputs = [ pkgs.nodePackages.prettier ];
+            }
+            ''
+              prettier --check ${self}/public/index.html
+              touch $out
+            '';
+        typos =
+          pkgs.runCommandLocal "run-typos"
+            {
+              src = self;
+              nativeBuildInputs = [ pkgs.typos ];
+            }
+            ''
+              typos ${self}
+              touch $out
+            '';
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default =
+          let
+            serve = pkgs.callPackage ./nix/serve.nix { };
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              fd
+              libwebp
+              nixfmt-rfc-style
+              # format: `$ prettier -w public/index.html`
+              nodePackages.prettier
+              serve
+              typos
+            ];
           };
-          devShells.default =
-            let
-              serve = pkgs.callPackage ./nix/serve.nix { };
-            in
-            pkgs.mkShell {
-              packages = with pkgs; [
-                fd
-                libwebp
-                nixfmt-rfc-style
-                # format: `$ prettier -w public/index.html`
-                nodePackages.prettier
-                serve
-                typos
-              ];
-            };
-          formatter = pkgs.nixfmt-rfc-style;
-        };
-      flake = {
-        # Put your original flake attributes here.
-      };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
     };
 }
