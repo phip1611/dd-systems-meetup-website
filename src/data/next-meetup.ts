a7e16fcc10ca4811f6762a7baee1eecea936d29a
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-05-04 15:42:52 +0200

    website: move next-meetup info to dedicated TS file

---
+++ src/data/next-meetup.ts
@@ -0,0 +1,15 @@
+export interface NextMeetup {
+  date: string;
+  time: string;
+  location: string;
+  registrationNote: string;
+  hostNote: string;
+}
+
+export const nextMeetup: NextMeetup = {
+  date: "Thu., May 21st, 2026",
+  time: "18:00 - 22:00",
+  location: "Genode Labs",
+  registrationNote: "Registration opens end of April.",
+  hostNote: "Open for more 2026 hosts - please reach out!",
+};
